165603818a732dc18d9f0c40b6bf288486826d34
rfttra <auliafitra2002@gmail.com>
2022-06-07 12:44:56 +0700

    Style Margin

--- resources/views/store/layouts/main.blade.php
+++ resources/views/store/layouts/main.blade.php
@@ -30,44 +30,44 @@
     <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 
-<body>
+<body style="margin: 0;">
     <!-- Topbar Start -->
-        <div class="row align-items-center py-3 px-xl-5">
-            <div class="col-lg-3 d-none d-lg-block">
-                <a href="" class="text-decoration-none">
-                    <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                            class="text-primary font-weight-bold border px-3 mr-1">K</span>Merch</h1>
-                </a>
-            </div>
-            @if(Request::is('login') OR Route::is('register'))
-            @else
-            <div class="col-lg-6 col-6 text-left">
-                <form action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for products">
-                        <div class="input-group-append">
-                            <span class="input-group-text bg-transparent text-primary">
-                                <i class="fa fa-search"></i>
-                            </span>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-lg-3 col-6 text-right">
-                <a href="" class="btn border">
-                    <i class="fas fa-heart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-                <a href="" class="btn border">
-                    <i class="fas fa-shopping-cart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-            </div>
-            @endif
+    <div class="row align-items-center py-3 px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <a href="" class="text-decoration-none">
+                <h1 class="m-0 display-5 font-weight-semi-bold"><span
+                        class="text-primary font-weight-bold border px-3 mr-1">K</span>Merch</h1>
+            </a>
         </div>
+        @if(Request::is('login') OR Route::is('register'))
+        @else
+        <div class="col-lg-6 col-6 text-left">
+            <form action="">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for products">
+                    <div class="input-group-append">
+                        <span class="input-group-text bg-transparent text-primary">
+                            <i class="fa fa-search"></i>
+                        </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-3 col-6 text-right">
+            <a href="" class="btn border">
+                <i class="fas fa-heart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+            <a href="" class="btn border">
+                <i class="fas fa-shopping-cart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+        </div>
+        @endif
+    </div>
     </div>
     <!-- Topbar End -->
-    
+
 
     <!-- Navbar Start -->
     <div class="container-fluid mb-3">
@@ -120,8 +120,8 @@
                             @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="nav-item nav-link">Register</a>
                             @endif
-                           
-                            
+
+
                             @endauth
                             @endif
                         </div>
