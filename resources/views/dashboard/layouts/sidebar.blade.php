10a74cbc98877f067db8913d43e4ff2523c0a05d
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2022-06-12 04:39:47 +0700

    MinorUpdateOnProduct

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -34,13 +34,13 @@
                 <a class="nav-link {{ Request::is('dashboard/product*') ? 'active' : ''}}" href="{{ '/dashboard/product' }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">                        
-                        <i class="fa fa-cubes text-warning text-sm opacity-10" aria-hidden="true"></i>
+                        <i class="fas fa-archive text-info text-sm opacity-10" aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Product</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="../pages/profile.html">
+                <a class="nav-link " href="#">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
@@ -57,7 +57,7 @@
                     <span class="nav-link-text ms-1">Store Page</span>
                 </a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link " href="../pages/sign-up.html">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -65,6 +65,6 @@
                     </div>
                     <span class="nav-link-text ms-1">Sign Up</span>
                 </a>
-            </li>
+            </li> --}}
         </ul>
 </aside>
